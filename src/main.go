c755afc8689d023dd7b5d5a76108b4d4ca22935c
kainlite <kainlite@gmail.com>
2024-11-06 12:40:03 -0300

    chore: fmt main.go

--- src/main.go
+++ src/main.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-  "context"
-  "os"
-  "time"
-  "fmt"
-  "net/http"
-  "encoding/json"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 
-  "database/sql"
-  _ "github.com/lib/pq"
+	"database/sql"
+	_ "github.com/lib/pq"
 
 	"github.com/redis/go-redis/v9"
 )
 
 // Postgres connection variables
 var (
-  host     = os.Getenv("DATABASE_HOST")
-  port     = os.Getenv("DATABASE_PORT")
-  user     = os.Getenv("DATABASE_USER")
-  password = os.Getenv("DATABASE_PASSWORD")
-  dbname   = os.Getenv("DATABASE_NAME")
+	host     = os.Getenv("DATABASE_HOST")
+	port     = os.Getenv("DATABASE_PORT")
+	user     = os.Getenv("DATABASE_USER")
+	password = os.Getenv("DATABASE_PASSWORD")
+	dbname   = os.Getenv("DATABASE_NAME")
 )
 
 // Redis connection variables
 var (
-  rhost = os.Getenv("REDIS_HOST")
-  rpassword = os.Getenv("REDIS_PASSWORD")
-  rport = os.Getenv("REDIS_PORT")
-	ctx = context.Background()
+	rhost     = os.Getenv("REDIS_HOST")
+	rpassword = os.Getenv("REDIS_PASSWORD")
+	rport     = os.Getenv("REDIS_PORT")
+	ctx       = context.Background()
 )
 
 // Types
 type RequestLogger struct {
-  Time time.Time
-  RemoteAddr string
-  StatusCode uint
-  Path string
+	Time       time.Time
+	RemoteAddr string
+	StatusCode uint
+	Path       string
 }
 
 type Health struct {
-  Status string
-  Database string
-  Cache string
+	Status   string
+	Database string
+	Cache    string
 }
 
 type PageVisits struct {
@@ -51,99 +51,99 @@ type PageVisits struct {
 }
 
 type PgStats struct {
-  Username string
-  Database string
-  ClientAddr string
-  State string
+	Username   string
+	Database   string
+	ClientAddr string
+	State      string
 }
 
 // Helper functions
 func logger(r *http.Request) {
-  u, err := json.Marshal(
-    RequestLogger{
-      Time: time.Now().UTC(), 
-      RemoteAddr: r.RemoteAddr, 
-      StatusCode: http.StatusOK, 
-      Path: r.URL.Path,
-    })
+	u, err := json.Marshal(
+		RequestLogger{
+			Time:       time.Now().UTC(),
+			RemoteAddr: r.RemoteAddr,
+			StatusCode: http.StatusOK,
+			Path:       r.URL.Path,
+		})
 
-  if err != nil {
-      panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  fmt.Printf("%v\n", string(u))
+	fmt.Printf("%v\n", string(u))
 }
 
 func errlogger(r *http.Request) {
-  u, err := json.Marshal(
-    RequestLogger{
-      Time: time.Now().UTC(), 
-      RemoteAddr: r.RemoteAddr, 
-      StatusCode: http.StatusNotFound, 
-      Path: r.URL.Path,
-    })
+	u, err := json.Marshal(
+		RequestLogger{
+			Time:       time.Now().UTC(),
+			RemoteAddr: r.RemoteAddr,
+			StatusCode: http.StatusNotFound,
+			Path:       r.URL.Path,
+		})
 
-  if err != nil {
-      panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  fmt.Printf("%v\n", string(u))
+	fmt.Printf("%v\n", string(u))
 }
 
 func healthcheck(db *sql.DB, redis *redis.Client) (bool, string) {
-  var status, database, cache string
+	var status, database, cache string
 
-  // Check database connection
-  if err := db.Ping(); err != nil {
-    fmt.Printf("db.Ping() error: %v\n", err)
-    database = "error"
-  } else {
-    database = "ok"
-  }
+	// Check database connection
+	if err := db.Ping(); err != nil {
+		fmt.Printf("db.Ping() error: %v\n", err)
+		database = "error"
+	} else {
+		database = "ok"
+	}
 
-  // Check cache connection
-  if _, err := redis.Ping(ctx).Result(); err != nil {
-    fmt.Printf("db.Ping() error: %v\n", err)
-    cache = "error"
-    } else {
-    cache = "ok"
-  }
+	// Check cache connection
+	if _, err := redis.Ping(ctx).Result(); err != nil {
+		fmt.Printf("db.Ping() error: %v\n", err)
+		cache = "error"
+	} else {
+		cache = "ok"
+	}
 
-  // Check status
-  if database == "ok" && cache == "ok" {
-    status = "ok"
-  } else {
-    status = "degraded"
-  }
+	// Check status
+	if database == "ok" && cache == "ok" {
+		status = "ok"
+	} else {
+		status = "degraded"
+	}
 
-  u, err := json.Marshal(
-    Health{
-      Status: status,
-      Database: database,
-      Cache: cache,
-    })
+	u, err := json.Marshal(
+		Health{
+			Status:   status,
+			Database: database,
+			Cache:    cache,
+		})
 
-  if err != nil {
-      panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  return status == "degraded", fmt.Sprintf("%v\n", string(u))
+	return status == "degraded", fmt.Sprintf("%v\n", string(u))
 }
 
 func increment_visits_counter(rc *redis.Client, path string) {
- 	_, err := rc.Incr(ctx, fmt.Sprintf("page_visits_%s", path)).Result()
+	_, err := rc.Incr(ctx, fmt.Sprintf("page_visits_%s", path)).Result()
 	if err != nil {
 		fmt.Println(err)
 	}
 
- 	_, err = rc.Incr(ctx, fmt.Sprintf("page_visits_total")).Result()
+	_, err = rc.Incr(ctx, fmt.Sprintf("page_visits_total")).Result()
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
 func stats(rc *redis.Client) string {
-  // TODO: Return early if status is degraded because of redis being down
+	// TODO: Return early if status is degraded because of redis being down
 	pattern := "page_visits_*"
 	keys, err := rc.Keys(ctx, pattern).Result()
 	if err != nil {
@@ -170,24 +170,24 @@ func stats(rc *redis.Client) string {
 }
 
 func pgstats(db *sql.DB) string {
-  // TODO: Return early if status is degraded because of postgres being down
-  rows, err := db.Query("SELECT COALESCE(usename, ''), COALESCE(datname, ''), client_addr, COALESCE(state, '') FROM pg_stat_activity;")
-  if err != nil {
-    fmt.Println(err)
-  }
-  defer rows.Close()
+	// TODO: Return early if status is degraded because of postgres being down
+	rows, err := db.Query("SELECT COALESCE(usename, ''), COALESCE(datname, ''), client_addr, COALESCE(state, '') FROM pg_stat_activity;")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
 
-  var pgstats []PgStats
-  for rows.Next() {
-      var usename, datname, client_addr, state string
+	var pgstats []PgStats
+	for rows.Next() {
+		var usename, datname, client_addr, state string
 
-      _ = rows.Scan(&usename, &datname, &client_addr, &state);
+		_ = rows.Scan(&usename, &datname, &client_addr, &state)
 
-      if len(usename) == 0 {
-        continue
-      }
-      pgstats = append(pgstats, PgStats{Username: usename, Database: datname, ClientAddr: client_addr, State: state})
-  }
+		if len(usename) == 0 {
+			continue
+		}
+		pgstats = append(pgstats, PgStats{Username: usename, Database: datname, ClientAddr: client_addr, State: state})
+	}
 
 	jsonData, err := json.Marshal(pgstats)
 	if err != nil {
@@ -198,70 +198,70 @@ func pgstats(db *sql.DB) string {
 }
 
 func main() {
-  mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-  // Connect to postgres
-  psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
+	// Connect to postgres
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
-  db, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    panic(err)
-  }
-  defer db.Close()
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-  // Connect to redis
-  redisClient := redis.NewClient(&redis.Options{
-    Addr:	  fmt.Sprintf("%s:%s", rhost, rport),
-    Password: rpassword,
-    DB:		  0,    
-    Protocol: 2, 
-  })
-  defer redisClient.Close()
+	// Connect to redis
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", rhost, rport),
+		Password: rpassword,
+		DB:       0,
+		Protocol: 2,
+	})
+	defer redisClient.Close()
 
-  // Routes
-  mux.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-    logger(r)
-    increment_visits_counter(redisClient, r.URL.Path)
-    degraded, result := healthcheck(db, redisClient)
-    if degraded {
-      w.WriteHeader(http.StatusTeapot)
-      fmt.Fprintf(w, result)
-    } else {
-      fmt.Fprintf(w, result)
-    }
-  })
+	// Routes
+	mux.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		logger(r)
+		increment_visits_counter(redisClient, r.URL.Path)
+		degraded, result := healthcheck(db, redisClient)
+		if degraded {
+			w.WriteHeader(http.StatusTeapot)
+			fmt.Fprintf(w, result)
+		} else {
+			fmt.Fprintf(w, result)
+		}
+	})
 
-  mux.HandleFunc("/api/stats", func(w http.ResponseWriter, r *http.Request) {
-    logger(r)
-    increment_visits_counter(redisClient, r.URL.Path)
-    fmt.Fprintf(w, stats(redisClient))
-  })
+	mux.HandleFunc("/api/stats", func(w http.ResponseWriter, r *http.Request) {
+		logger(r)
+		increment_visits_counter(redisClient, r.URL.Path)
+		fmt.Fprintf(w, stats(redisClient))
+	})
 
-  mux.HandleFunc("/api/pgstats", func(w http.ResponseWriter, r *http.Request) {
-    logger(r)
-    increment_visits_counter(redisClient, r.URL.Path)
-    fmt.Fprintf(w, pgstats(db))
-  })
+	mux.HandleFunc("/api/pgstats", func(w http.ResponseWriter, r *http.Request) {
+		logger(r)
+		increment_visits_counter(redisClient, r.URL.Path)
+		fmt.Fprintf(w, pgstats(db))
+	})
 
-  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    increment_visits_counter(redisClient, r.URL.Path)
-    if r.URL.Path != "/" {
-      errlogger(r)
-      w.WriteHeader(http.StatusNotFound)
-      fmt.Fprintf(w, "Error 404")
-    } else {
-      logger(r)
-      fmt.Fprintf(w, "Hi Koronet Team.")
-    }
-  })
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		increment_visits_counter(redisClient, r.URL.Path)
+		if r.URL.Path != "/" {
+			errlogger(r)
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "Error 404")
+		} else {
+			logger(r)
+			fmt.Fprintf(w, "Hi Koronet Team.")
+		}
+	})
 
-  // Start the web server
-  fmt.Println("Starting server on port 8000")
-  err = http.ListenAndServe(":8000", mux)
-  if err != nil {
-    fmt.Printf("server error: %s\n", err)
-    panic(err)
-  }
+	// Start the web server
+	fmt.Println("Starting server on port 8000")
+	err = http.ListenAndServe(":8000", mux)
+	if err != nil {
+		fmt.Printf("server error: %s\n", err)
+		panic(err)
+	}
 }
